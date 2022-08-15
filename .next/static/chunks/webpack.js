d118d4a58f06a16df227152aa15efa0cbaae8fb2
Sidoryx <siddhantban02@gmail.com>
2022-08-16 01:07:38 +0530

    Made a few more changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1ec15e20e6e2a034"; }
+/******/ 		__webpack_require__.h = function() { return "2b73c0a48f64c990"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
