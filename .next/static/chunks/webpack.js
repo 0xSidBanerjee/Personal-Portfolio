446535511c556efd1854c8f9311747989da3b806
Sidoryx <siddhantban02@gmail.com>
2022-08-19 13:11:56 +0530

    Changed dark mode logo and button animations

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7db4230dab7e376f"; }
+/******/ 		__webpack_require__.h = function() { return "0847f29e9c634a28"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Button/index.js
+++ components/Button/index.js
@@ -9,7 +9,7 @@ const Button = ({ children, type, onClick, classes }) => {
         onClick={onClick}
         type="button"
         className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg ${
-          theme === "dark" ? "bg-white text-black" : "bg-black text-white"
+          theme === "dark" ? "bg-royal-purple text-white" : "bg-royal-purple text-white"
         }  transition-all duration-300 ease-out first:ml-0 hover:scale-105 active:scale-100`}
       >
         {children}
@@ -22,7 +22,7 @@ const Button = ({ children, type, onClick, classes }) => {
       type="button"
       className={`text-sm tablet:text-base p-1 laptop:p-2 m-1 laptop:m-2 rounded-lg flex items-center transition-all ease-out duration-400 hover:text-royal-purple ${
         theme === "dark"
-          ? "hover:bg-slate-100 text-white"
+          ? "hover: text-white"
           : "hover:"
       } hover:scale-105 active:scale-100  tablet:first:ml-0 ${classes}`}
     >
